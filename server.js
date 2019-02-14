6add36fe59b5bbab6c265c398758fae95cacf728
Eduardo Martinez <eduardo.martinez@pressganey.com>
2019-02-13 19:10:25 -0600

    Deployment of full application

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(cors());
 
-app.use('/prime', PrimeNumberController);
+app.use('/api', PrimeNumberController);
 
 app.listen(process.env.PORT || 5000, () =>
   console.log(`Server started on port ${process.env.PORT}`)
